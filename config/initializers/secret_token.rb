f7ac731fa9bf3300da2afb2c76282cb971f9c012
Kurtis Brown <kurtis@kurtisebear.com>
2014-01-02 21:22:52 +0000

    inital repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitFuck::Application.config.secret_key_base = 'e2850e25fd9f8dac58d6f0db183ab6d58c288ab2f41cf9f285afa3cc12988d55d3f5c760c1f1995f710564dedfec4efe29ce439638d36dfc1337973e1dea0d0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitFuck::Application.config.session_store :cookie_store, key: '_twit_fuck_session'
