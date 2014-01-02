8ff8b3d08e6e98cc3278cbda9e29d44b50e7d0cf
Kurtis Brown <kurtis@kurtisebear.com>
2014-01-02 21:24:57 +0000

    edit secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TwitFuck::Application.config.secret_key_base = 'e2850e25fd9f8dac58d6f0db183ab6d58c288ab2f41cf9f285afa3cc12988d55d3f5c760c1f1995f710564dedfec4efe29ce439638d36dfc1337973e1dea0d0a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+TwitFuck::Application.config.secret_key_base = secure_token
